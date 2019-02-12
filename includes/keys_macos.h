54488b9c3cc34c0b67c1316157ee9ec46ea15f66
Iipal <malkoleyplay@gmail.com>
2019-02-12 19:03:40 +0200

    boos key on macos

--- includes/keys_macos.h
+++ includes/keys_macos.h
@@ -6,41 +6,42 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 12:46:42 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/02/10 09:33:06 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/02/12 18:24:32 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef KEYS_MACOS_H
 # define KEYS_MACOS_H
 
-# define KEY_PRESSED	2
-# define KEY_RELEASE	2
+# define KEY_PRESSED    2
+# define KEY_RELEASE    2
 
-# define ESC			53
+# define ESC            53
 
-# define KEY_C			8
+# define LSHIFT         257
+# define KEY_C          8
 
-# define KEY_W			13
-# define KEY_A			0
-# define KEY_S			1
-# define KEY_D			2
+# define KEY_W          13
+# define KEY_A          0
+# define KEY_S          1
+# define KEY_D          2
 
-# define KEY_T			17
-# define KEY_G			5
-# define KEY_Y			16
-# define KEY_H			4
+# define KEY_T          17
+# define KEY_G          5
+# define KEY_Y          16
+# define KEY_H          4
 
-# define KEY_I			34
-# define KEY_P			35
+# define KEY_I          34
+# define KEY_P          35
 
-# define ARROW_UP		126
-# define ARROW_DOWN		125
-# define ARROW_RIGHT	124
-# define ARROW_LEFT		123
+# define ARROW_UP       126
+# define ARROW_DOWN     125
+# define ARROW_RIGHT    124
+# define ARROW_LEFT     123
 
-# define PLUS_NUMPAD	69
-# define PLUS_KEYBOARD	24
-# define MINUS_NUMPAD	78
-# define MINUS_KEYBOARD	27
+# define PLUS_NUMPAD    69
+# define PLUS_KEYBOARD  24
+# define MINUS_NUMPAD   78
+# define MINUS_KEYBOARD 27
 
 #endif
