a3625fb8822351cc332d385d92ae04a61d02da8e
Iipal <malkoleyplay@gmail.com>
2019-02-12 19:02:58 +0200

    improved key hooks & deleted FoV binds

--- srcs/wolf_key_hooks.c
+++ srcs/wolf_key_hooks.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 14:44:03 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/02/12 13:44:55 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/02/12 19:01:57 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,37 +22,33 @@ int			wolf_killwindow(t_env *env)
 static void	add_action_wasd(int key, t_env *env)
 {
 	if (key == KEY_W)
-		wolf_move(env, MOVE_INC);
+		wolf_move(env, ISRB ? (MOVE_BOOST * MOVE_INC) : MOVE_INC);
 	if (key == KEY_A)
-		wolf_rotate(RC, _RAD(ROT_INC));
+		wolf_rotate(RC, _RAD(ISRB ? (ROT_BOOST * ROT_INC) : ROT_INC));
 	if (key == KEY_S)
-		wolf_move(env, -MOVE_INC);
+		wolf_move(env, ISRB ? (MOVE_BOOST * -MOVE_INC) : -MOVE_INC);
 	if (key == KEY_D)
-		wolf_rotate(RC, _RAD(-ROT_INC));
+		wolf_rotate(RC, _RAD(ISRB ? (ROT_BOOST * -ROT_INC) : -ROT_INC));
 }
 
 static void	add_action_arrows(int key, t_env *env)
 {
 	if (key == ARROW_UP)
-		wolf_move(env, MOVE_INC);
+		wolf_move(env, ISRB ? (MOVE_BOOST * MOVE_INC) : MOVE_INC);
 	if (key == ARROW_DOWN)
-		wolf_move(env, -MOVE_INC);
+		wolf_move(env, ISRB ? (MOVE_BOOST * -MOVE_INC) : -MOVE_INC);
 	if (key == ARROW_LEFT)
-		wolf_rotate(RC, _RAD(ROT_INC));
+		wolf_rotate(RC, _RAD(ISRB ? (ROT_BOOST * ROT_INC) : ROT_INC));
 	if (key == ARROW_RIGHT)
-		wolf_rotate(RC, _RAD(-ROT_INC));
+		wolf_rotate(RC, _RAD(ISRB ? (ROT_BOOST * -ROT_INC) : -ROT_INC));
 }
 
 int			wolf_key_hooks(int key, t_env *env)
 {
 	if (key == ESC)
 		wolf_killwindow(env);
-	if (key == PLUS_NUMPAD || key == PLUS_KEYBOARD)
-		RC->plane = (fpoint) {RC->plane.y + FOV_INC, RC->plane.x + FOV_INC};
-	if (key == MINUS_NUMPAD || key == MINUS_KEYBOARD)
-		if (RC->plane.y - FOV_INC >= FOV_MIN
-		&& RC->plane.x - FOV_INC >= FOV_MIN)
-			RC->plane = (fpoint) {RC->plane.y - FOV_INC, RC->plane.x - FOV_INC};
+	if (key == LSHIFT)
+		ISRB ? (ISRB = false) : (ISRB = true);
 	add_action_wasd(key, env);
 	add_action_arrows(key, env);
 	wolf_rendering_rc(env);
