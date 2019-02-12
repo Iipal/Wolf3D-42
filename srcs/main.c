9bca2512e5f6a2d28024913c63263f6d8ce412f8
Iipal <malkoleyplay@gmail.com>
2019-02-12 19:04:19 +0200

    added hooks for mouse

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 14:19:04 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/02/12 14:05:27 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/02/12 18:44:49 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,9 @@ static void	add_init_rc_n_randomize_pos(t_env *env)
 
 static void	add_mlx_hooks(t_env *env)
 {
+	mlx_hook(WPTR, MOUSE_DOWN, MOUSE_MASK, wolf_mouse_press, env);
+	mlx_hook(WPTR, MOUSE_UP, MOUSE_MASK, wolf_mouse_release, env);
+	mlx_hook(WPTR, MOUSE_MOVE, MOUSE_MASK, wolf_mouse_moves, env);
 	mlx_hook(WPTR, KEY_PRESSED, KEY_RELEASE, wolf_key_hooks, env);
 	mlx_hook(WPTR, WIN_EXT, WIN_EXTM, wolf_killwindow, env);
 	wolf_rendering_rc(env);
