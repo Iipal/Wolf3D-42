a3af064f6bb8b515e5519b8703630b909b1faa46
Iipal <malkoleyplay@gmail.com>
2019-02-20 23:12:11 +0200

    added short wolf3d main description

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,25 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 14:19:04 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/02/19 18:36:14 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/02/20 23:01:50 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/wolf3d.h"
+
+static void	wolf_usage(void)
+{
+	_MSGN("Usage:");
+	_MSGN("  [W | S] - Moving forward | backward(or Arrows [UP | DOWN])");
+	_MSGN("  [A | D] - Rotate left | right"
+		"(also Arrows [LEFT | RIGHT] or mouse)");
+	_MSGN("  [T] - Toggle Textured\\Flat rendering method.");
+	_MSGN("  [M] - Toggle mini-map drawing.");
+	_MSGN("  [F] - Toggle Fog drawing.");
+	_MSGN("  [C] - Switch Fog colors");
+	_MSGN("  [LShift] - BOOST");
+	_MSGN("  [ESC] - exit.");
+}
 
 int			main(int argc, string argv[])
 {
@@ -22,8 +36,23 @@ int			main(int argc, string argv[])
 	_NOTIS(E_ALLOC, env = (t_env*)malloc(sizeof(t_env)), exit(EXIT_FAILURE), 0);
 	_NOTIS(E_ALLOC, wolf_init(env), wolf_free(&env), EXIT_FAILURE);
 	_NOTIS(E_FILER, wolf_readnsave(*argv, env), exit(EXIT_FAILURE), 0);
-	wolf_init_rc_n_randomize_pos(env);
+	wolf_setup_rc(env);
+	wolf_usage();
 	wolf_rendering_rc(env);
 	wolf_sdl_events_loop(env);
 	SDL_Quit();
 }
+
+/*
+**	Fast, short description:
+**
+**	For beginning need to validate executable path for correct textures loading
+**		(macro on line 19).
+**	Next step is validate arguments for program (only 1 must to be).
+**	Allocating memory for 'main' structure & initialize SDL2/Loading textures.
+**	After that we can save our map from file to `env->map`.
+**	Then we can set-up default settings for RayCastring structure
+**		& randomize position for bonus.
+**	If all was great we can print usage & start RC rendering proccess.
+**	And start main 'endless' loop where we will hook events & re-drawing all.
+*/
