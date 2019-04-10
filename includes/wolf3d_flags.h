389c7c6f5a058ddb0c7f19b2551836069cbe8bf1
Iipal <malkoleyplay@gmail.com>
2019-04-10 13:56:05 +0300

    .. -> \t

--- includes/wolf3d_flags.h
+++ includes/wolf3d_flags.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 16:26:52 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/10 11:22:33 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/10 13:55:51 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,12 @@
 # define F_NO_SOUND         "-no-sound"
 
 /*
-**  Turned off main menu screen on start.
+**	Turned off main menu screen on start.
 */
 # define F_NO_MENU          "-no-menu"
 
 /*
-**  Turned off by default fog rendering.
+**	Turned off by default fog rendering.
 */
 # define F_NO_FOG           "-no-fog"
 
@@ -54,47 +54,47 @@
 # define F_DIFF_SIDE        "-world-sides"
 
 /*
-**  Turned on by default rotate player camera without left mouse click.
+**	Turned on by default rotate player camera without left mouse click.
 */
 # define F_ALWAYS_MOUSE     "-use-mouse"
 
 /*
-**  Short flag equals to -help.
+**	Short flag equals to -help.
 */
 # define FS_HELP            "-h"
 
 /*
-**  Short flag equals to -dbg-info.
+**	Short flag equals to -dbg-info.
 */
 # define FS_DEBUG_INFO      "-d"
 
 /*
-**  Short flag equals to -no-border.
+**	Short flag equals to -no-border.
 */
 # define FS_WIN_NO_BORDER   "-nb"
 
 /*
-**  Short flag equals to -no-border.
+**	Short flag equals to -no-border.
 */
 # define FS_NO_SOUND        "-ns"
 
 /*
-**  Short flag equals to -no-menu.
+**	Short flag equals to -no-menu.
 */
 # define FS_NO_MENU         "-nm"
 
 /*
-**  Short flag equals to -no-fog.
+**	Short flag equals to -no-fog.
 */
 # define FS_NO_FOG          "-nf"
 
 /*
-**  Short flag equals to -world-sides.
+**	Short flag equals to -world-sides.
 */
 # define FS_DIFF_SIDE       "-ws"
 
 /*
-**  Short flag equals to -dbg-info.
+**	Short flag equals to -dbg-info.
 */
 # define FS_ALWAYS_MOUSE    "-um"
 
