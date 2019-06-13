807c2c633edab7c4088b039fc23bba4cc4790801
Iipal <malkoleyplay@gmail.com>
2019-06-13 10:07:20 +0300

    Update libft & refactor file ierarchy

--- srcs/rendering/wolf_torch.c
+++ srcs/rendering/wolf_torch.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/16 22:08:27 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/13 09:46:30 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/13 10:04:27 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	wolf_rendering_torch(t_torch *torch, Uint32 *win_pxls, uint8_t fog_freq)
 		{
 			curr_tex_pxl = torch->tex[torch_frame]
 				.pixels[p.y * torch->tex[torch_frame].surf->w + p.x];
-			if (curr_tex_pxl != 0xff000000 || curr_tex_pxl != 0x00)
+			if (curr_tex_pxl != 0xff000000L && curr_tex_pxl != 0x0L)
 				if (WIN_Y > p.y + torch_shift.y && WIN_X > p.x + torch_shift.x)
 					win_pxls[(p.y + torch_shift.y)
 						* WIN_X + (p.x + torch_shift.x)] = curr_tex_pxl;
