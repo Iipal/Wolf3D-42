c01fb6379508adaad57f219c8357af12a8301b10
Iipal <malkoleyplay@gmail.com>
2019-06-13 09:48:02 +0300

    improved in torch rendering current put pixel range validation

--- srcs/rendering/wolf_torch.c
+++ srcs/rendering/wolf_torch.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/16 22:08:27 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/13 09:44:46 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/13 09:46:30 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,8 @@ void	wolf_rendering_torch(t_torch *torch, Uint32 *win_pxls, uint8_t fog_freq)
 			curr_tex_pxl = torch->tex[torch_frame]
 				.pixels[p.y * torch->tex[torch_frame].surf->w + p.x];
 			if (curr_tex_pxl != 0xff000000 || curr_tex_pxl != 0x00)
-				win_pxls[(p.y + torch_shift.y) * WIN_X + (p.x + torch_shift.x)]
-					= curr_tex_pxl;
+				if (WIN_Y > p.y + torch_shift.y && WIN_X > p.x + torch_shift.x)
+					win_pxls[(p.y + torch_shift.y)
+						* WIN_X + (p.x + torch_shift.x)] = curr_tex_pxl;
 		}
 }
